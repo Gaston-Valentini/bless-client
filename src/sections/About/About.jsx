bf8cc39a259d07e3d30f4de62246476f411d5280
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-04-22 15:55:17 +0200

    Add: About section added.

---
+++ src/sections/About/About.jsx
@@ -0,0 +1,30 @@
+import style from "./About.module.css";
+import image from "../../assets/images/tamara.jpg";
+import { FaInstagram } from "react-icons/fa";
+
+export default function About() {
+    return (
+        <section className={style.container}>
+            <div className={style.content}>
+                <div className={style.contentData}>
+                    <div className={style.contentDataTitle}>
+                        <hr />
+                        <p>Tamara</p>
+                        <hr />
+                    </div>
+                    <p className={style.contentDataText}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo autem ipsam, voluptates porro cum ea vel accusamus unde. Ullam porro ea blanditiis, necessitatibus deleniti sapiente. Hic facere dolorem eaque consectetur! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo autem ipsam, voluptates porro cum ea vel accusamus unde. Ullam porro ea blanditiis, necessitatibus deleniti sapiente. Hic facere dolorem eaque consectetur! Lorem, ipsum dolor sit amet consectetur
+                        adipisicing elit. Quo autem ipsam, voluptates porro cum ea vel accusamus unde. Ullam porro ea blanditiis, necessitatibus deleniti sapiente. Hic facere dolorem eaque consectetur!
+                    </p>
+                    <a href="#" className={style.contentDataSocial}>
+                        <FaInstagram />
+                        <p>Instagram</p>
+                    </a>
+                </div>
+                <div className={style.contentImage}>
+                    <img src={image} />
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,11 @@
 import HomeHeader from "../../sections/HomeHeader/HomeHeader";
+import About from "../../sections/About/About";
 
 export default function HomeView() {
     return (
         <section>
             <HomeHeader />
+            <About />
         </section>
     );
 }
