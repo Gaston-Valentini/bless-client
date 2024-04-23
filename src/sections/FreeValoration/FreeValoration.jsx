b6ba503217c2fb3eb5cf33ef909e392aeafe3632
Gaston-Valentini <gastonvalentiniruiz@gmail.com>
2024-04-23 09:13:09 +0200

    Add: FreeValoration section added.

---
+++ src/sections/FreeValoration/FreeValoration.jsx
@@ -0,0 +1,20 @@
+import style from "./FreeValoration.module.css";
+import { Link } from "react-router-dom";
+
+export default function FreeValoration() {
+    return (
+        <section className={style.container}>
+            <div className={style.overlay}>
+                <p className={style.title}>Tu primera valoración es GRATIS</p>
+                <p className={style.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione recusandae fugiat atque, et rerum veritatis
+                    placeat sit a. Perspiciatis expedita aperiam dolor optio, deleniti mollitia maiores officiis ipsa dignissimos
+                    voluptates.
+                </p>
+                <Link to="#" className={style.link}>
+                    Contáctanos
+                </Link>
+            </div>
+        </section>
+    );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,6 +1,7 @@
 import HomeHeader from "../../sections/HomeHeader/HomeHeader";
 import About from "../../sections/About/About";
 import Services from "../../sections/Services/Services";
+import FreeValoration from "../../sections/FreeValoration/FreeValoration";
 
 export default function HomeView() {
     return (
@@ -8,6 +9,7 @@ export default function HomeView() {
             <HomeHeader />
             <About />
             <Services />
+            <FreeValoration />
         </section>
     );
 }
