5a3fd38a5bfa32bf49a5b97647909d88b3296d56
Gaston-Valentini <gastonvalentiniruiz@gmail.com>
2024-04-25 12:03:24 +0200

    Add: Map component added.

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,12 +1,14 @@
 import ViewHeader from "../../components/ViewHeader/ViewHeader";
 import headerBackground from "../../assets/images/contact-header.jpg";
 import ContactForm from "../../sections/ContactForm/ContactForm";
+import Map from "../../sections/Map/Map";
 
 export default function ContactView() {
     return (
         <section>
             <ViewHeader title="Contacto" subtitle="Disbonibles para lo que necesites" background={headerBackground} />
             <ContactForm />
+            <Map />
         </section>
     );
 }
